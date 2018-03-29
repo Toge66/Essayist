58045a027c4e2b6dabcb90a85ba1bfbb253e477d
toge <toge0223@163.com>
2018-03-29 12:07:10 +0800

    修改webpack4分包配置

--- webpack/CodeSplitting/webpack.config.js
+++ webpack/CodeSplitting/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const webpack = require('webpack')
 const HTMLWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
@@ -9,10 +10,26 @@ module.exports = {
   plugins: [
     new HTMLWebpackPlugin({
       title: 'code splitting'
-    })
+    }),
+    // new webpack.optimize.CommonsChunkPlugin({
+    //   name: 'common'
+    // })
   ],
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        commons: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendor',
+          chunks: 'all'
+        }
+      }
+    }
+  },
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+
+  devtool: 'cheap-module-eval-source-map'
 }
